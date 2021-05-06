253eb5331ffdccd1c9b54a1e54c795e660b2c401
sergii <serjmitaki@gmail.com>
2021-05-06 10:30:22 +0300

    add main screen again

--- lib/app/main_screen.dart
+++ lib/app/main_screen.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 
 import '../menu/my_drawer.dart';
-
 
 class MainScreen extends StatefulWidget {
   @override
@@ -10,24 +8,52 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     //ImportSongs().loadSongsFromJson();  - if needed to insert songs to database from json file
     return Scaffold(
       appBar: AppBar(
         title: Text('ICOC'),
-
       ),
       drawer: Drawer(
-        child:  MyDrawer(),),
-
-      body: Center( child:
-      Text('Main Screen', style: Theme.of(context).textTheme.headline3,),
+        child: MyDrawer(),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            elevation: 4,
+            margin: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                      ),
+                      child: Image.asset('assets/images/verse_of_day.jpeg'),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Icon(Icons.favorite),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('Отрывок дня'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
