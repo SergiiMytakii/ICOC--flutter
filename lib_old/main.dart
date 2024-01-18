bf543fd7d8475e16c40595eece65f0e18b4ce77b
sergii <serjmitaki@gmail.com>
2024-01-18 11:25:25 +0200

    rename lib

---
+++ lib_old/main.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+import 'package:firebase_core/firebase_core.dart';
+import 'index.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  await Firebase.initializeApp();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  final GetStorage box = GetStorage();
+  String? appLocale;
+  if (await box.read('locale') != null) {
+    appLocale = box.read('locale');
+  }
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+
+  runApp(MyApp(appLocale: appLocale, savedThemeMode: savedThemeMode));
+}
+
+class MyApp extends StatelessWidget {
+  final String? appLocale;
+  final AdaptiveThemeMode? savedThemeMode;
+  MyApp({Key? key, this.appLocale, this.savedThemeMode}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      initial: savedThemeMode ?? AdaptiveThemeMode.system,
+      light: myLightTheme,
+      dark: myDarkTheme,
+      builder: (light, dark) => OKToast(
+        child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          locale:
+              appLocale != null ? languagesLocales[appLocale] : window.locale,
+          fallbackLocale: Locale('ru', 'RU'),
+          theme: light,
+          darkTheme: dark,
+          translations: Messages(),
+          title: 'ICOC',
+          home: MainScreen(),
+          getPages: Pages.getPages(),
+        ),
+      ),
+    );
+  }
+}
