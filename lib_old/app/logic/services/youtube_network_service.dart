75a4102fa585c9220560222aaff975a5554b35ae
sergii <serjmitaki@gmail.com>
2024-01-23 17:33:14 +0200

    delete lib_old

--- lib_old/app/logic/services/youtube_network_service.dart
+++
@@ -1,7 +0,0 @@
-import '../../../index.dart';
-
-class YoutubeNetworkService extends GetConnect {
-  static const _apiUrl = 'https://www.googleapis.com/youtube/v3/';
-
-  GetHttpClient httpClient = GetHttpClient(baseUrl: _apiUrl);
-}
