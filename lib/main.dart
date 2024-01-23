19440a8ddbabb69eb02b65a92d4018096f19ad6d
sergii <serjmitaki@gmail.com>
2024-01-23 12:28:03 +0200

    extract MultiblocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:icoc/core/bloc/bible_study_bloc/bible_study_bloc.dart';
-import 'package:icoc/core/bloc/favorite_song_status_bloc/favorite_songs_bloc.dart';
-import 'package:icoc/core/bloc/favorite_songs_list_bloc/favorite_songs_bloc.dart';
-import 'package:icoc/core/bloc/feedback_bloc/feedback_bloc.dart';
-import 'package:icoc/core/bloc/notifications_bloc/notifications_bloc.dart';
-import 'package:icoc/core/bloc/q&a_bloc/q&a_bloc.dart';
-import 'package:icoc/core/bloc/video_bloc/video_bloc.dart';
+import 'package:icoc/core/bloc/multibloc_provider.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'constants.dart';
-import 'core/bloc/font_size_bloc/font_size_bloc.dart';
-import 'core/bloc/songs_bloc/songs_bloc.dart';
 import 'core/helpers/shared_preferences_helper.dart';
 import 'presentation/routes/app_routes.dart';
 import 'theme.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -53,37 +44,7 @@ class MyApp extends StatelessWidget {
       initial: savedThemeMode ?? AdaptiveThemeMode.dark,
       light: myLightTheme,
       dark: myDarkTheme,
-      builder: (light, dark) => MultiBlocProvider(
-        providers: [
-          BlocProvider<SongsBloc>(
-            create: (BuildContext context) => SongsBloc(),
-          ),
-          BlocProvider<FontSizeBloc>(
-            create: (BuildContext context) =>
-                FontSizeBloc()..add(FontSizeRequested()),
-          ),
-          BlocProvider<FavoriteSongsListBloc>(
-            create: (BuildContext context) => FavoriteSongsListBloc(),
-          ),
-          BlocProvider<FavoriteSongStatusBloc>(
-            create: (BuildContext context) => FavoriteSongStatusBloc(),
-          ),
-          BlocProvider<BibleStudyBloc>(
-            create: (BuildContext context) => BibleStudyBloc(),
-          ),
-          BlocProvider<QandABloc>(
-            create: (BuildContext context) => QandABloc(),
-          ),
-          BlocProvider<NotificationsBloc>(
-            create: (BuildContext context) => NotificationsBloc(),
-          ),
-          BlocProvider<VideoBloc>(
-            create: (BuildContext context) => VideoBloc(),
-          ),
-          BlocProvider<FeedbackBloc>(
-            create: (BuildContext context) => FeedbackBloc(),
-          ),
-        ],
+      builder: (light, dark) => MyMultiblocProvider(
         child: OverlaySupport.global(
           toastTheme: ToastThemeData(textColor: Colors.white),
           child: MaterialApp(
